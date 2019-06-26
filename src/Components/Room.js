7d225f46c7da5960218e43180b95910b6aee60bc
jacobzielinski <32741770+jacobzielinski@users.noreply.github.com>
2019-06-26 09:33:58 +0100

    hello

--- src/Components/Room.js
+++ src/Components/Room.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Chatkit from "@pusher/chatkit-client";
 
+
+
+
+
+
 class Room extends React.Component {
   state = {
     messages: [],
     value: "",
     messageToSend: "",
-    user: undefined
+    user: undefined,
+    usersInRoom: 0,
+    usersAreActive: "offline"
   };
+
+  
 
   componentDidMount() {
     const tokenProvider = new Chatkit.TokenProvider({
@@ -17,7 +26,7 @@ class Room extends React.Component {
 
     const chatManager = new Chatkit.ChatManager({
       instanceLocator: "v1:us1:e26280f8-acac-4da9-9e2a-80cd549547f8",
-      userId: "AB",
+      userId: "jz",
       tokenProvider: tokenProvider
     });
 
@@ -27,22 +36,30 @@ class Room extends React.Component {
         this.setState({ user: currentUser });
         currentUser.subscribeToRoomMultipart({
           roomId: currentUser.rooms[0].id,
+    
           hooks: {
             onMessage: message => {
               let oldMessages = this.state.messages;
               oldMessages.push(message);
               this.setState({ messages: oldMessages });
-              console.log(
-                `${message.senderId} said:`,
-                message.parts[0].payload.content
-              );
+              this.setState({ usersInRoom: currentUser.users.length})
+              
+             
+            },
+             onPresenceChanged: (state, user) => {
+               if (user.name === chatManager.userId){
+              console.log(`User ${user.name} is ${state.current}`)
+              this.setState({usersAreActive: state.current})
+               }
             }
-          }
+          },
+          messageLimit: 20,
         });
 
         currentUser
           .fetchMultipartMessages({
-            roomId: "19442511"
+            roomId: "19442511",
+           
           })
           .then(messages => {
             this.setState({
@@ -55,19 +72,14 @@ class Room extends React.Component {
       })
       .catch(err => console.log(err));
   }
-
+  
   send = e => {
     e.preventDefault();
     this.state.user.sendSimpleMessage({
       text: this.state.value,
       roomId: this.state.user.rooms[0].id
     });
-    // let oldMessages = this.state.messages;
-    // oldMessages.push(this.state.value);
-    // this.setState({ messages: oldMessages });
-
-    // oldMessages.push(...newMessage);
-    // this.setState({ messages: oldMessages });
+   
   };
 
   handleChange = e => {
@@ -78,15 +90,16 @@ class Room extends React.Component {
     console.log(this.state.messages);
     return (
       <div>
-        <h1>Room</h1>
+        <h1>Room ({this.state.usersInRoom} users)</h1>
         <form action="">
           <input type="text" onChange={this.handleChange} />
           <input type="submit" onClick={this.send} />
         </form>
+      
         {this.state.messages.map(message => {
           return (
             <li>
-              {message.senderId} said "{message.parts[0].payload.content}" @{" "}
+              {message.senderId} {this.state.usersAreActive} said "{message.parts[0].payload.content}" @{" "}
               {message.createdAt}
             </li>
           );
